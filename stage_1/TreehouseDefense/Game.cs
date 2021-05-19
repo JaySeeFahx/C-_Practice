fb231c58994105e0f7f16b4dcb9323a7a6a5aa00
Jacob Clark <jacobclark08@gmail.com>
2021-05-19 16:29:21 -0400

    adding project files

---
+++ stage_1/TreehouseDefense/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5); //variable of type Map with name map, creates a Map object with a given Width and Height
+
+        }
+    }
+}
---
+++ stage_1/TreehouseDefense/Map.cs
@@ -0,0 +1,20 @@
+﻿namespace TreehouseDefense
+{
+    class Map
+    {
+        public readonly int Width; //field of Width for new class of Map
+        public readonly int Height; //field of Height for new class of Map
+
+        public Map(int width, int height) //constructor method that applies width and height to the fields of Width and Height
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public bool OnMap(Point point) //public method, type BOOLEAN, name OnMap, assigns the TYPE Point to the VARIABLE point & checks to see if given point is on the map
+        {
+            return point.X >= 0 && point.X < Width && 
+                   point.Y >= 0 && point.Y < Height;
+        }
+    }
+}
