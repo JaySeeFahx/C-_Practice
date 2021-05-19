3a6f0e6675f5b22d0e57b83aa42e5e79e090e83c
Jacob Clark <jacobclark08@gmail.com>
2021-05-19 18:37:16 -0400

    Updated Game and Path files

--- stage_1/TreehouseDefense/Game.cs
+++ stage_1/TreehouseDefense/Game.cs
@@ -8,6 +8,40 @@ namespace TreehouseDefense
         {
             Map map = new Map(8, 5); //variable of type Map with name map, creates a Map object with a given Width and Height
 
+            try
+            {
+
+                Path path = new Path(
+                    new [] {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                    }
+                );
+
+                MapLocation location = path.GetLocationAt(8);
+                if (location != null)
+                {
+                    Console.WriteLine($"{location.X},{location.Y}");
+                }
+            }
+            catch (OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (TreehouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unhandled Exception: {ex}");
+            }
         }
     }
 }
--- stage_1/TreehouseDefense/Path.cs
+++ stage_1/TreehouseDefense/Path.cs
@@ -1,7 +1,17 @@
 ﻿namespace TreehouseDefense
 {
     class Path
-    { 
-    
+    {
+        private readonly MapLocation[] _path;
+        
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
